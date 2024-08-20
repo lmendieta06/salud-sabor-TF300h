fa5fc5dcc9d0f4b74e7a0710bd0988b6e1d30dbc
Gabrzcode <agugabriel94@gmail.com>
2024-08-20 16:44:33 -0500

    registro,login,nav

--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -5,14 +5,20 @@ import { ClientComponent } from './pages/client/client.component';
 import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuRestaurantComponent } from './pages/menu-restaurant/menu-restaurant.component';
-import { NoEncontradoComponent } from './pages/no-encontrado/no-encontrado.component';
 import { RecipesTipsComponent } from './pages/recipes-tips/recipes-tips.component';
 import { RestaurantsComponent } from './pages/restaurants/restaurants.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NoEncontradoComponent } from './pages/no-encontrado/no-encontrado.component';
+
+
 
 export const routes: Routes = [
     {path:"", component: HomeComponent, title: "Salud & Sabor"},
     {path:"restaurantes", component: RestaurantsComponent, title: "Restaurantes"},
+    {path:"registro", component: RegisterComponent, title: "Registro"},
+    {path:"login", component:LoginComponent, title: "Log in"},
     {path:"contactanos", component:ContactUsComponent, title:"Contactanos"},
     {path:"administrador", component:AdminComponent, title:"Administrador"},
     {path:"cliente", component:ClientComponent, title:"Salud & Sabor"},
