e32aaf20dd4f6ac331d077e0ff3cca6bec20b0f2
Gabrzcode <agugabriel94@gmail.com>
2024-08-22 12:47:33 -0500

    ajustes y creacion de cart

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
+import { PLATFORM_ID, Inject } from '@angular/core';
+import { AfterViewInit, Component,} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { CartComponent } from './components/cart/cart.component';
+import AOS from "aos";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'FRONTEND';
+export class AppComponent implements AfterViewInit {
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private router: Router // Inyecta Router aquí
+  ) {}
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      // Inicializa AOS
+      AOS.init({
+        duration: 800, // Duración en milisegundos
+        easing: 'ease-in-out', // Efecto de aceleración
+        delay: 100, // Retraso en milisegundos
+        
+      });
+
+      // Reinicia el scroll al cambiar de página
+      this.router.events.pipe(
+        filter(event => event instanceof NavigationEnd)
+      ).subscribe(() => {
+        window.scrollTo(0, 0);
+      });
+    
+    
+    }
+  }
 }
