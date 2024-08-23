13de59becb50810b61e1b96e8fb2970389fa1871
Gabrzcode <agugabriel94@gmail.com>
2024-08-23 04:39:16 -0500

    navigation responsive y accesibilidad check

--- FRONTEND/src/app/components/navegation/navegation.component.ts
+++ FRONTEND/src/app/components/navegation/navegation.component.ts
@@ -1,24 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-navegation',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navegation.component.html',
-  styleUrl: './navegation.component.css'
+  styleUrls: ['./navegation.component.css'] // Corregido a styleUrls
 })
 export class NavegationComponent {
-  toggleMenu() {
-    const menu = document.querySelector('.dropdown-menu');
+  isDropdownVisible: boolean = false;
+  private hideTimeout: any;
 
-    if (menu?.classList.contains('show')) {
-      menu.classList.remove('show');
-      menu.classList.add('hide');
-    } else {
-      menu?.classList.remove('hide');
-      menu?.classList.add('show');
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    // Si el clic no es dentro del dropdown o el icono de usuario, oculta el dropdown
+    const target = event.target as HTMLElement;
+    if (!target.closest('.dropdown') && !target.closest('.userIcon')) {
+      this.hideDropdown();
     }
   }
 
+  @HostListener('mouseenter', ['$event'])
+  onMouseEnter(event: Event) {
+    clearTimeout(this.hideTimeout);
+    this.isDropdownVisible = true;
+  }
+
+  @HostListener('mouseleave', ['$event'])
+  onMouseLeave(event: Event) {
+    this.hideTimeout = setTimeout(() => {
+      this.isDropdownVisible = false;
+    }, 500);
+  }
+
+  hideDropdown() {
+    clearTimeout(this.hideTimeout);
+    this.isDropdownVisible = false;
+  }
 }
