204ebd32af638f00813b2def42c48f6b5479c917
Gabrzcode <agugabriel94@gmail.com>
2024-08-22 23:30:29 -0500

    ajustes animaciones de entrada y salida

--- FRONTEND/src/app/components/navegation/navegation.component.ts
+++ FRONTEND/src/app/components/navegation/navegation.component.ts
@@ -9,5 +9,16 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navegation.component.css'
 })
 export class NavegationComponent {
+  toggleMenu() {
+    const menu = document.querySelector('.dropdown-menu');
+
+    if (menu?.classList.contains('show')) {
+      menu.classList.remove('show');
+      menu.classList.add('hide');
+    } else {
+      menu?.classList.remove('hide');
+      menu?.classList.add('show');
+    }
+  }
 
 }
