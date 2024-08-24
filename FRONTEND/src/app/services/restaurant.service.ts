adfac71b75033d03cbc4697b2d9acdf7267f2db0
Laura Mendieta <laumendieta06@gmail.com>
2024-08-24 00:40:32 -0500

    [CREATE] SERVICIO RESTAURANTE

---
+++ FRONTEND/src/app/services/restaurant.service.ts
@@ -0,0 +1,68 @@
+import { inject,Injectable} from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+    providedIn:'root'
+})
+
+export class RestaurantService{
+
+    constructor(){}
+
+    httpClient = inject(HttpClient);
+
+    API_URL_GET = "http://localhost:2000/restaurants/";
+    API_URL_GET_CATEGORY = "http://localhost:2000/restaurants/:categoria";
+    API_URL_GET_CITY = "http://localhost:2000/restaurants/:ciudad";
+    API_URL_POST = "http://localhost:2000/restaurants/";
+    API_URL_PUT = "http://localhost:2000/restaurants/:_id";
+    API_URL_DELETE = "http://localhost:2000/restaurants/:_id";
+
+    getRestaurants(){
+        return this.httpClient.get(this.API_URL_GET);
+    }
+
+    getRestaurantByCategory(categoria:string){
+        return this.httpClient.get(`${this.API_URL_GET_CATEGORY}/${categoria}`);
+    }
+
+    getRestaurantByCity(ciudad:string){
+        return this.httpClient.get(`${this.API_URL_GET_CATEGORY}/${ciudad}`);
+    }
+
+    postRestaurant(nombre:string, ciudad:string, correoElectronico:string, categoria:string, descripcion:string, direccion:string, menu:Object, logo:string){
+        
+        const infoRestaurant ={
+            nombre:nombre,
+            ciudad:ciudad,
+            correoElectronico:correoElectronico,
+            categoria:categoria,
+            descripcion:descripcion,
+            direccion:direccion,
+            menu:menu,
+            logo:logo
+        }
+
+        return this.httpClient.post(this.API_URL_POST,infoRestaurant);
+    }
+
+    putRestaurant(nombre:string, ciudad:string, correoElectronico:string, categoria:string, descripcion:string, direccion:string, menu:Object, logo:string, id:string){
+        
+        const infoRestaurant ={
+            nombre:nombre,
+            ciudad:ciudad,
+            correoElectronico:correoElectronico,
+            categoria:categoria,
+            descripcion:descripcion,
+            direccion:direccion,
+            menu:menu,
+            logo:logo
+        }
+
+        return this.httpClient.put(`${this.API_URL_PUT}/${id}`, infoRestaurant);
+    }
+
+    deleteRestaurant(id:string){
+        return this.httpClient.delete(`${this.API_URL_DELETE}/${id}`);
+    }
+}
