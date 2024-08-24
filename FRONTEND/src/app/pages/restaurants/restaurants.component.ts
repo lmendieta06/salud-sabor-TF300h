ffade08621c848ff753d6c65e800f9ca2a9710ac
Laura Mendieta <laumendieta06@gmail.com>
2024-08-23 22:32:52 -0500

    [CREATE] SERVICIO Y REDENRIZACION PRODUCTOS - BUSCAR PRODUCTO

--- FRONTEND/src/app/pages/restaurants/restaurants.component.ts
+++ FRONTEND/src/app/pages/restaurants/restaurants.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavegationComponent } from '../../components/navegation/navegation.component';
 import { RouterLink } from '@angular/router';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ChatBotComponent } from '../../components/chat-bot/chat-bot.component';
+import { RestaurantService } from '../../services/restaurant.service.js';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-restaurants',
   standalone: true,
-  imports: [NavegationComponent, RouterLink, FooterComponent, ChatBotComponent],
+  imports: [NavegationComponent, RouterLink, FooterComponent, ChatBotComponent, CommonModule],
   templateUrl: './restaurants.component.html',
   styleUrl: './restaurants.component.css'
 })
-export class RestaurantsComponent {
 
+export class RestaurantsComponent {
+  
+  restaurantsService = inject(RestaurantService);
+
+  allRestaurants : any [] = [];
+
+  getRestaurants(){
+    this.restaurantsService.getRestaurants().subscribe((res: any) => {
+      console.log(res); // Verifica cómo es la estructura de la respuesta
+      if (res) {
+        this.allRestaurants = res.restaurants
+      } else {
+        console.error("Hubo un error");
+      }
+    });
+  }
+
+    // Metodo que permite que se rendericen (muestren en pantalla) los productos con el sitio web
+
+  ngOnInit(){
+    // Decir que lo renderice
+    this.getRestaurants();
+  }
 }
